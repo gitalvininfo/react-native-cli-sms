d858032faab01780f5c34d36ba53dceb7e2860cc
Alvin <yanson.alvin.info@gmail.com>
2022-11-08 18:22:57 +0800

    starter

---
+++ App.js
@@ -0,0 +1,113 @@
+import React,{useState} from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  NativeModules,
+  PermissionsAndroid,
+} from 'react-native';
+let DirectSms = NativeModules.DirectSms;
+const DirectSMSTest = () => {
+const [mobileNumber, setMobileNumber] = useState('');
+const [bodySMS, setBodySMS] = useState('This is Test Message Only.');
+const sendDirectSms = async () => {
+   if (mobileNumber) {
+      try { 
+         const granted = await PermissionsAndroid.request(     
+              PermissionsAndroid.PERMISSIONS.SEND_SMS,
+              {
+                title: 'Sending SMS App Sms Permission',
+                message:'Send SMS App needs access to your inbox so you can send messages in  background',
+                buttonNegative: 'Ask Me Later',
+                buttonNegative: 'Cancel',
+                buttonPositive: 'OK',
+              },
+           );
+           if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+             DirectSms.sendDirectSms(mobileNumber, bodySMS);
+             console.log('called sms')
+           } else {
+             alert('SMS permission denied');
+           }
+      } catch (error) {
+          console.warn(error);
+         alert(error);
+      }
+    }
+  };
+   return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <Text style={styles.titleText}> 
+                React Native Bridge Example for Android to Send  
+                Direct SMS
+          </Text>
+          <Text style={styles.titleTextsmall}>
+               Enter Recipients Number
+          </Text>
+          <TextInput
+              value={mobileNumber}
+              onChangeText={mobileNumber => 
+                          setMobileNumber(mobileNumber)}
+              placeholder={'Enter Conatct Number to Call'}
+              keyboardType="numeric"
+              style={styles.textInput}
+          />
+          <Text style={styles.titleTextsmall}>Enter SMS Body</Text>
+          <TextInput
+              value={bodySMS}
+              onChangeText={bodySMS => setBodySMS(bodySMS)}
+              placeholder={'Enter SMS body'}
+              style={styles.textInput}
+           />
+          <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.buttonStyle}
+              onPress={sendDirectSms}
+           >
+              <Text style={styles.buttonTextStyle}>Send  
+                Message</Text>
+         </TouchableOpacity>
+       </View>
+     </SafeAreaView>
+   );
+};
+export default DirectSMSTest;
+const styles = StyleSheet.create({
+    container: {
+       flex: 1,
+       backgroundColor: 'white',
+       padding: 10,
+       textAlign: 'center',
+    },
+    titleText: {
+       fontSize: 22,
+       textAlign: 'center',
+       fontWeight: 'bold',
+       marginBottom: 16,
+    },
+    titleTextsmall: {
+       marginVertical: 8,
+       fontSize: 16,
+    },
+    buttonStyle: {
+       justifyContent: 'center',
+       marginTop: 15,
+       padding: 10,
+       backgroundColor: '#8ad24e',
+    },
+    buttonTextStyle: {
+       color: '#fff',
+       textAlign: 'center',
+    },
+    textInput: {
+       height: 40,
+       borderColor: 'gray',
+       borderWidth: 1,
+       width: '100%',
+       paddingHorizontal: 10,
+    },
+ });
